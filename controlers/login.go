ac50c5208441d2670ec6739bddbaa2dcecb70b71
royanqodri <royanqodri@gmail.com>
2023-05-17 08:22:31 +0700

    menambahakan fitur Read User lain

--- controlers/login.go
+++ controlers/login.go
@@ -7,11 +7,11 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func GetLoginUsers(db *sql.DB, No_telepon int, password string) (bool, error) {
+func GetLoginUsers(db *sql.DB, No_telp int, password string) (bool, error) {
 	// Query the database to check if the user exists
 	query := "SELECT COUNT(*) FROM user WHERE No_telepon = ? AND password = ?"
 	var count int
-	err := db.QueryRow(query, No_telepon, password).Scan(&count)
+	err := db.QueryRow(query, No_telp, password).Scan(&count)
 	if err != nil {
 		return false, fmt.Errorf("Login: %v", err)
 	}
